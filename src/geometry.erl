c875eccd0b866c1e740ce7743ba68b68acdc1b03
AirSmile <66414161@qq.com>
2014-05-05 11:16:23 +0800

    测试提交

--- src/geometry.erl
+++ src/geometry.erl
@@ -15,5 +15,4 @@
 -export([area/1]).
 %% area 函数里面有一个参数,这参数是一个原子,这原子里面有3个变量.
 area({rectangle, Width, Ht}) -> Width * Ht;   %% 将Width * Ht = 长 * 高 =面积
-area({circle, R}) -> 3.141592 * R * R.        %% 计算圆形的面积
-
+area({circle, R}) -> 3.141592 * R * R.        %% 计算圆形的面积测试
